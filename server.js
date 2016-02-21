ee2ed987a8f13eef310c34d93071c00f20ec43ed
ckm100 <kufogbemawuli1@gmail.com>
2016-02-21 12:33:04 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+var http = require("http"),
+    path = require("path"),
+    express = require("express"),
+    app = express(),
+    moment = require("moment"),
+    c,
+    decode,
+    u;
+
+app.set('port', (process.env.PORT || 9000))
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/[0-9]{1,}/", function (req, res) {
+
+    c = moment.unix(+req.url.substring(1, req.url.length));
+
+    res.json({
+        "unix": parseInt(req.url.substring(1, req.url.length)),
+        natural: c.format("MMMM Do, YYYY")
+    });
+
+});
+
+app.get("/([a-zA-Z0-9]*|(%20|\.|\-)[a-zA-Z0-9]*(%20|\.|\-)[a-zA-Z0-9]*)/", function (req, res) {
+
+    decode = decodeURI(req.url.substring(1, req.url.length));
+    u = new Date(decode).getTime() / 1000;
+
+    if (/[a-zA-Z](\s|\.|\-)/.test(decode) === false) {
+
+        res.json({
+            "unix": null,
+            "natural": null
+        });
+
+    } else {
+
+        res.json({
+            "unix": u,
+            "natural": (moment.unix(u).format("MMMM Do, YYYY") === "Invalid date") ? null : moment.unix(u).format("MMMM Do, YYYY")
+        });
+
+    }
+
+});
+
+app.listen(app.get('port'), function () {
+
+    console.log("Listening on port ", app.get('port'));
+
+});
